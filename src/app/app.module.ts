e48827efb5fdfb5f3711cd965e9d735ffc326b6b
flaffa <mzabsas1@gmail.com>
2023-05-08 14:26:10 -0300

    primmer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { AcercadeComponent } from './component/acercade/acercade.component';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { ProyectoComponent } from './component/proyecto/proyecto.component';
+import { FooterComponent } from './component/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    BannerComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectoComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
